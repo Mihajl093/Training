45a91a87f7d79b19ab2fa05b2fbdde2cb22de4e7
Mihajl093 <mihajlodavidcuk@gmail.com>
2022-10-21 22:40:17 +0300

    add Calculation class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import static Lesson5.Calculator.isPrime;
+
 public class Main {
     public static void main(String[] args) {
+        var result = isPrime(9, 3);
 
-        System.out.println("Hello world!");
+        System.out.println("The result is - " + result);
     }
 }
